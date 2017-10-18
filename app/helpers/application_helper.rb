e355ad049291a7c3dc69037e4e4e7a40b4b81c57
Pham Van Huy <phamvanhuy14@gmail.com>
2017-10-18 17:10:26 +0700

    user sig_in log_in log_out

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title page_title
+    base_title = I18n.t "layouts.application.app_name"
+    if page_title.nil?
+      base_title
+    else
+      page_title
+    end
+  end
 end
