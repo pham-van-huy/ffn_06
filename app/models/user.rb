aa0841ecaf14e8a8ed95c2dea288ed35321684d4
Pham Van Huy <phamvanhuy14@gmail.com>
2017-10-18 17:10:26 +0700

    user register

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :bets, dependent: :destroy
   has_many :new, dependent: :destroy
+
   has_secure_password
+  validates :email, presence: true, uniqueness: true
 end
