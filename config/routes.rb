93096fba52ea1d91af870fe8485512222cb2d649
Pham Van Huy <phamvanhuy14@gmail.com>
2017-10-19 11:14:31 +0700

    show news in homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root "application#home"
+  root "news#index"
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+  resources :news, only: [:show]
 end
