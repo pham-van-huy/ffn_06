14e9bdfb8f59cfae1da422ad5f771a389a3aeb07
Pham Van Huy <phamvanhuy14@gmail.com>
2017-11-08 10:02:01 +0700

    admin team and fix login by facebook

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,10 +1,12 @@
 class Team < ApplicationRecord
   attr_accessor :score
   has_many :players, dependent: :destroy
-  has_many :matchs_one, dependent: :destroy, foreign_key: :team1_id, class_name: :Match
-  has_many :matchs_two, dependent: :destroy, foreign_key: :team2_id, class_name: :Match
+  has_many :matchs_one, foreign_key: :team1_id, class_name: :Match, dependent: :destroy
+  has_many :matchs_two, foreign_key: :team2_id, class_name: :Match, dependent: :destroy
   belongs_to :country
-  belongs_to :country
+  validates :name, presence: true, uniqueness: true, length: {maximum: Settings.limit_max_title}
+  validates :description, length: {maximum: Settings.limit_min_content}
+  mount_uploader :logo, LogoTeamUploader
   scope :desc, ->{order "created_at DESC"}
   scope :get_search, ->(key){joins(:country).where("countries.name REGEXP ?", key)}
 end
